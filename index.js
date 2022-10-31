4ddfb2764131c00961939b02625da650d76b5b35
Yetunde Alese <yaalese1@gmail.com>
2022-10-31 19:45:20 -0400

    commit tree e009e8f3acdea6124022576eeca4005315105a4c

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+
+ let num1 = 31
+ let num2 = 2
+ 
+ let multiply = (num1 * num2)
+
+
+let random = (Math.floor (Math.random() *10) +1)
+ 
+let num3 = 16
+let num4 = 6
+let mod = (num3 % num4)
+
+let max = (Math.max)
+max = (1,2,10,20);
+
+   
+ 
+ 
